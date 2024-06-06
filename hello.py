b397c4918cb3333bc45eaf40dfc9524100a6902b
Rafael Schwambach <rafaelschwambach@gmail.com>
2024-06-06 08:50:13 -0300

    Implementamos o hello world multi línguas

--- hello.py
+++ hello.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-# Comentário acima chamado de SHEBANG. o env passa as variáveis do terminal
+Dependendo da língua configurada no ambiente, o programa exibe a mensagem 
+correspondente.
 
-# Este programa imprime Hello, World
-print("Hello, World!")
- 
+Usage:
+
+Tenha a variável LANG devidamente configurada. Ex.:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Rafael Schwambach"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG","en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "pt_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+print(msg)
