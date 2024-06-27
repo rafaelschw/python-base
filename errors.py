2c87873f126bf5d51e8df0f92fb933d8d8412697
Rafael Schwambach <rafaelschwambach@gmail.com>
2024-06-27 18:28:52 -0300

    Erro: EAFP, try, except, finally, raise

--- errors.py
+++ errors.py
@@ -2,17 +2,22 @@
 import os
 import sys
 
-# LBYL - Look Before You Leap
+# EAFP - Easy to Ask Forgiveness than Permission
 
-if os.path.exists("names.txt"):
-    input("...") # Race Condition
-    names = open("names.txt").readlines()
-else:
-    print("[Error] File names.txt not found")
+try:
+    names = open("names.txt").readlines() # FileNotFoundError
+except FileNotFoundError as e: # ('apenas except:' é um Bare except - captura qualquer erro. Tratamento de erro tem que ser bem específico)
+    print(f"{str(e)}.")
     sys.exit(1)
-
-if len(names) >= 3:
-    print(names[2])
+    sys.exit(1)
+    # TODO: Usar retry
 else:
+    print("Sucesso!!")
+finally:
+    print("Execute isso sempre!")
+
+try:
+    print(names[2])
+except:
     print("[Error] Missing name in the list")
     sys.exit(1)
